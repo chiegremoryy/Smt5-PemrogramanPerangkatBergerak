85512a7f3e336a070635eb27bf72adebe1d492d0
chiegremoryy <chiescode@gmail.com>
2024-10-11 23:55:41 +0700

    update

---
+++ week1/bin/latihandart.dart
@@ -0,0 +1,10 @@
+import 'package:latihandart/latihandart.dart' as latihandart;
+
+void main() {
+  int nilai = 90;
+  if (nilai > 80) {
+    print('Lulus');
+  } else {
+    print('Tidak Lulus');
+  }
+}
---
+++ week3/dart_project2/README.md
@@ -0,0 +1,3 @@
+# dartproject2
+
+A new Flutter project.
