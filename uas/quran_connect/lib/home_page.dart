50335b65fadc93f15375725375abc0abce09a70e
chiegremoryy <chiescode@gmail.com>
2024-11-22 19:54:49 +0700

    home page-fix

--- uas/quran_connect/lib/home_page.dart
+++ uas/quran_connect/lib/home_page.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage(), // Pastikan HomePage di sini
+      home: const HomePage(),
     );
   }
 }
